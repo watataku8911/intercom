650b0aeea222e7854f60e576c6babbff3230654d
watataku8911 <watato.takuya@gmail.com>
2022-03-08 16:52:30 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Button msg="ボタンを押してね。" @push="push()" />
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button
+  },
+  setup() {
+    const push = () => {
+      const audio = new Audio(require('@/assets/intercom.mp3'))
+      audio.play();
+    }
+
+    return {
+      push
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="button" @click="push">
+    {{ msg }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["push"],
+  setup(_, context) {
+    const push = () => {
+      context.emit("push");
+    };
+
+    return { push };
+  },
+};
+</script>
+
+<style scoped>
+.button {
+  border-radius: 10px;
+  padding: 2.5%;
+  font-size: 1em;
+  background-color: #71ff71;
+  color: #111;
+  border-style: none;
+  box-shadow: 3px 3px 3px 3px #ddd;
+}
+
+.button:hover {
+  box-shadow: 0px 0px 0px 0px #ddd;
+}
+
+.button:disabled {
+  box-shadow: 0px 0px 0px 0px #ddd;
+  background: #efefef;
+  opacity: 0.8;
+}
+</style>
